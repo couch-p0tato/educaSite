1515107c43ccf9953491f6e6aee439517bde9f0b
couch-p0tato <126980774+couch-p0tato@users.noreply.github.com>
2023-03-06 00:00:01 +0100

    New edited JS

--- main.js
+++ main.js
@@ -22,6 +22,31 @@ faqs.forEach(faq => {
     })
 })
 
+// JS for Tablet Media Queries
+
+// show/hide nav menu
+
+const menu = document.querySelector(".nav-menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+// close
+
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav);
+
+
 // SwiperJS
 
 var swiper = new Swiper(".mySwiper", {
@@ -38,25 +63,3 @@ var swiper = new Swiper(".mySwiper", {
         }
     }
 });
-
-
-// JS for Tablet Media Queries
-
-// show/hide nav menu
-const menu = document.querySelector(".nav-menu");
-const menuBtn = document.querySelector("#open-menu-btn");
-const closeBtn = document.querySelector("#close-menu-btn");
-
-menuBtn.addEventListener('click', () => {
-    menu.style.display = "flex";
-    closeBtn.style.display = "inline-block";
-    menuBtn.style.display = "none";
-})
-
-const closeNav = () => {
-    menu.style.display = "none";
-    closeBtn.style.display = "none";
-    menuBtn.style.display = "inline-block";
-}
-
-closeBtn.addEventListener('click', closeNav);
